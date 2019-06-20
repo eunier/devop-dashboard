85493c212a0bc80a6404240ba991a1e525f8fe25
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-06-20 17:56:15 -0400

    Add socket.io

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.send('<h1>test</>');
+});
+
+io.on('connectino', socket => {
+  console.log('socket io connected');
 });
 
 http.listen(3000, () => {
