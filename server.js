4ed2b9d7475f1057990fd22f58173cc429d9f6ce
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-06-20 23:36:57 -0400

    Add client

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('<h1>test</>');
 });
 
+let testCnt = 0;
+
 io.on('connectino', socket => {
   console.log('socket io connected');
+
+  socket.on('subscribeToTimer', interval => {
+    console.log(`client is subscribing to timer with interval, ${interval}`);
+
+    setInterval(() => {
+      socket.emit('timer', ++testCnt);
+    }, interval);
+  });
 });
 
-http.listen(3000, () => {
-  console.log('listening on port 3000');
+const port = 4000;
+http.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
